d3fa92386614c224ddd564c7e667b02c0637480c
brambooo <bram_reinold@hotmail.com>
2016-07-04 14:46:16 +0200

    Added asset folder 'handige_klassen' where I upload all the important classes in, that i like to reuse

--- formulieren/afbeelding_upload_formulier/afbeelding_uploaden_formulier.php
+++ formulieren/afbeelding_upload_formulier/afbeelding_uploaden_formulier.php
@@ -1,5 +1,12 @@
 <?php
+    // Formulier afhandelen
+    if(isset($_POST['submit'])) {
+        try {
 
+        } catch(Exception $ex) {
+            $ex;
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +16,20 @@
     <title>Afbeelding uploaden via formulier</title>
 </head>
 <body>
-    
+    <h1>Upload een afbeelding</h1>
+    <?php
+        if(isset($_POST['submit'])) { ?>
+            <!-- Toon afbeelding met bestaandsnaam -->
+            <?php } ?>
+
+    <!-- Formulier -->
+    <form action="<?php htmlentities( $_SERVER['PHP_SELF'] ) ?>" method="post" enctype="multipart/form-data">
+        <label for="">Afbeelding uploaden:</label>
+        </br>
+        <input type="file" name="afbeelding">
+        </br>
+        </br>
+        <input type="submit" name="submit" value="Uploaden">
+    </form>
 </body>
 </html>
---
+++ handige_klassen/FotoUploader.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Bram
+ * Date: 4-7-2016
+ * Time: 14:43
+ */
+class FotoUploader
+{
+    // Attributes
+    private $uploadFile;
+    private $mineType;
+
+    // Constructor
+
+    // Methods
+}
